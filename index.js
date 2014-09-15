289b04feb909f184a97cf41e73b169d480e2367e
wangjeaf <wangjeaf@gmail.com>
2014-09-15 16:08:10 +0800

    chain operation

--- index.js
+++ index.js
@@ -14,24 +14,27 @@ function format(args) {
   return msg
 }
 
-function wrap() {
+(function () {
   console._oldLog = console.log
   console._prefix = ''
   console.log = function() {
-      var msg = format(arguments)
-      console._oldLog.apply(this, [this._prefix + msg.white])
+    var msg = format(arguments)
+    console._oldLog.apply(this, [this._prefix + msg.white])
+    return this
   }
 
   console._oldWarn = console.warn
   console.warn = function() {
-      var msg = format(arguments)
-      console._oldWarn.apply(this, [this._prefix + msg.yellow])
+    var msg = format(arguments)
+    console._oldWarn.apply(this, [this._prefix + msg.yellow])
+    return this
   }
 
   console._oldError = console.error
   console.error = function() {
-      var msg = format(arguments)
-      console._oldError.apply(this, [this._prefix + msg.red])
+    var msg = format(arguments)
+    console._oldError.apply(this, [this._prefix + msg.red])
+    return this
   }
 
   console.dir = function() {
@@ -39,18 +42,23 @@ function wrap() {
     if (!obj) {
       return
     }
-    var msg = JSON.stringify(obj, null, 2);
+    var msg = JSON.stringify(obj, null, 2)
     msg = msg.replace(/(\n\s*)("\w+")(\s*:)/g, function(msg, msg2, msg3, msg4) {
-      return msg2 + msg3.green + msg4;
+      return msg2 + msg3.green + msg4
     })
     console._oldLog(msg)
+    return this
   }
+
   console.config = function(config) {
-      this._prefix = config.prefix || ''
+    this._prefix = config.prefix || ''
+    return this
   }
-}
 
+  console.prefix = function(prefix) {
+    this._prefix = prefix || ''
+    return this
+  }
+})()
 
-wrap()
-
-module.exports = console;
+module.exports = console
