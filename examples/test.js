b4c3d84cca1a66dce78a16647c2a37e7d26cc8b1
wangjeaf <wangjeaf@gmail.com>
2014-09-15 16:01:56 +0800

    add new test

--- examples/test.js
+++ examples/test.js
@@ -1,6 +1,18 @@
-// require('iconsole')
-// console.config ({
-//   'prefix': 'red'
-// })
+//require('iconsole').config({
+//    prefix: '[iconsole] '
+//})
 
-console.log('1321'.red)
+console.log('test log %d', 222)
+console.warn('test warn %s', 'fsafdas')
+console.error('test error')
+
+console.dir({
+   a: '1',
+   b: 2,
+   c: {
+    d: 'fdafdaaf\nfdafda',
+    e: {
+        f: 'afdafdsa'
+    }
+   }
+})
